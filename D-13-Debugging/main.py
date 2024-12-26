edc004c8d8dd2f2ef9241be07cc07736c830b00b
Dulshan Chamika <dulshanchamika7@gmail.com>
2024-12-26 23:49:40 +0530

    Update main.py

--- D-13-Debugging/main.py
+++ D-13-Debugging/main.py
@@ -1,8 +1,10 @@
-try:
-    age = int(input("How old are you?"))
-except ValueError:
-    print("Please try again with a numerical number like 15")
-    age = int(input("How old are you?"))
+# try:
+#     age = int(input("How old are you?"))
+# except ValueError:
+#     print("Please try again with a numerical number like 15")
+#     age = int(input("How old are you?"))
+#
+# if age > 18:
+#     print(f"You can drive at {age}.")
 
-if age > 18:
-    print(f"You can drive at {age}.")
+import random
