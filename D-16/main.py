f234a7b7225a460bb6161060c38cc7105b4c1c4b
Dulshan Chamika <dulshanchamika7@gmail.com>
2025-01-17 23:16:50 +0530

    Create main.py

---
+++ D-16/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+print(tim)
+tim.shape("turtle")
+tim.color("coral")
+tim.forward(200)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
