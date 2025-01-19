7dac0437fe43d8448007f9957d82f52b136cf515
Dulshan Chamika <dulshanchamika7@gmail.com>
2025-01-19 23:36:38 +0530

    Update main.py

--- D-16/main.py
+++ D-16/main.py
@@ -1,11 +1,19 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# print(tim)
+# tim.shape("turtle")
+# tim.color("coral")
+# tim.forward(200)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-tim = Turtle()
-print(tim)
-tim.shape("turtle")
-tim.color("coral")
-tim.forward(200)
+from prettytable import PrettyTable
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+print(table)
